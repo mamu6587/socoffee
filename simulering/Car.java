0dbbae4736b4ac15daf167d838de984e59a7c325
saraback <saramyback@gmail.com>
2014-11-14 13:51:15 +0100

    Comments in Car

--- simulering/Car.java
+++ simulering/Car.java
@@ -15,7 +15,13 @@ public class Car {
     public Car(){
 	System.out.println("Invalid arguments for Car constructor");
 }
-
+    /**
+     * Creates an instance of a Car object 
+     *
+     * @param bornTime an int representing the time this car was created
+     * @param currentPosition which CarPosition this Car should be initialized to
+     * @param turn  a boolean to know whether the car is supposed to turn
+     */
     public Car(int bornTime, CarPosition currentPosition,  boolean turn){
     	this.bornTime = bornTime;
     	this.Turn = turn;
@@ -23,31 +29,57 @@ public class Car {
     }
 
 //konstruktor och get- och set-metoder
-    
+    /**
+     * Gets the bornTime from the car
+     *
+     * @return the bornTime of the car
+     */
     public int getTime(){
     	return this.bornTime;
     }
     
+    /**
+     * Gets the current position from the car
+     *
+     * @return the current position of the car
+     */
+
     public CarPosition getPosition(){
     	return this.currentPosition;
     }
-    
+    /**
+     * 
+     *
+     * @return true if the car should turn, false otherwise
+     */
     public boolean getTurn(){
     	return this.Turn;
     }
-    
+    /**
+     * Sets the current osition for the Car
+     *
+     * @param position the new position
+     */
     public void setPosition(CarPosition position){
     	this.currentPosition = position;
     }
-    
+    /**
+     * Sets the turn attribute
+     *
+     * @param turn true for turn, false for going straight
+     */
     public void setTurn(boolean turn){
     	this.Turn = turn;
     	}
     
   
-    //...
     
-
+    
+    /**
+     * Prints a representation of a car
+     *
+     * @return the String "X"
+     */
    
     public String toString() {
     	
